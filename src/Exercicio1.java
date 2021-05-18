3d04a80fad70fecc630a3d467770a331668a1c48
Vinicius Matos <vmatos12@gmail.com>
2021-05-17 21:34:03 -0300

    Adicionando exercicio 1

--- src/Exercicio1.java
+++ src/Exercicio1.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
 
 public class Exercicio1 {
+	public static void main(String args []) {
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		int quantidade =0;
+		
+		System.out.println("Para entrar no castelo você deve acertar a quantidade de feijões. Qual é o seu palpite?");
+		quantidade = leitor.nextInt();
+		
+		while(quantidade != 1238) {
+			
+			System.out.println("Errooou! Digite novamente");
+			quantidade = leitor.nextInt();
+		}
+		
+		System.out.println("Você Acertou! Parabéns! Entrada permitida");
+	}
 
 }
